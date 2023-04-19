f459b979ee53d080b3eac9a533abba7f7890aa8b
Heytor Mesquita <heytormesquita@gmail.com>
2023-04-19 00:27:35 -0300

    Adiciona configuracao para usar lombok

--- src/main/java/com/msr/logfluxo/domain/model/Cliente.java
+++ src/main/java/com/msr/logfluxo/domain/model/Cliente.java
@@ -1,5 +1,10 @@
 package com.msr.logfluxo.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Cliente {
 	
 	private Long id;
@@ -7,30 +12,4 @@ public class Cliente {
 	private String email;
 	private String telefone;
 	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-		
 }
