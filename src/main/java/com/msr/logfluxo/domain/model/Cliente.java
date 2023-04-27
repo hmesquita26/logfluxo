633cfe06c542e49eef62b27019c6451766d312ee
Heytor Mesquita <heytormesquita@gmail.com>
2023-04-27 01:06:11 -0300

    adiciona anotations do jakarta persistence e lombok

--- src/main/java/com/msr/logfluxo/domain/model/Cliente.java
+++ src/main/java/com/msr/logfluxo/domain/model/Cliente.java
@@ -1,15 +1,28 @@
 package com.msr.logfluxo.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 }
