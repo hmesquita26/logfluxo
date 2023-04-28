f09db1f02d90a24fcfc0892efa7714c079dffdb1
Heytor Mesquita <heytormesquita@gmail.com>
2023-04-28 02:01:02 -0300

    adiciona anotations para validacao dos atributos da classe Cliente

--- src/main/java/com/msr/logfluxo/domain/model/Cliente.java
+++ src/main/java/com/msr/logfluxo/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,9 +22,17 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotEmpty
+	@Size(min = 3, max = 50)
 	private String nome;
+	
+	@NotEmpty
+	@Email
 	private String email;
 	
+	@NotEmpty
+	@Size(min = 0, max = 20)
 	@Column(name = "fone")
 	private String telefone;
 	
