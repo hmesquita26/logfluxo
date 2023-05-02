d1731356495c28d7aa128d24d7474bb0d1c84d88
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-02 18:27:03 -0300

    adiciona validacao de cliente para cadastrar solicitacao

--- src/main/java/com/msr/logfluxo/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/com/msr/logfluxo/domain/service/SolicitacaoEntregaService.java
@@ -4,9 +4,9 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 
+import com.msr.logfluxo.domain.model.Cliente;
 import com.msr.logfluxo.domain.model.Entrega;
 import com.msr.logfluxo.domain.model.StatusEntrega;
-import com.msr.logfluxo.domain.repository.ClienteRepository;
 import com.msr.logfluxo.domain.repository.EntregaRepository;
 
 import jakarta.transaction.Transactional;
@@ -17,12 +17,14 @@ import lombok.AllArgsConstructor;
 @Service
 public class SolicitacaoEntregaService {
 
-	private ClienteRepository clienteRepositpry;
+	private CatalogoClienteService catalogoClienteService;
 	private EntregaRepository entregaRepository;
 	
 	@Transactional
 	public Entrega solicitar(Entrega entrega) {
-
+		Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
+		
+		entrega.setCliente(cliente);
 		entrega.setStatus(StatusEntrega.PENDENTE);
 		entrega.setDataPedido(LocalDateTime.now());
 		
