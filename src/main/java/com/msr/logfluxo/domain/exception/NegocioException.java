f221883d4d5194bfc95d97ebd713f70dc3a374f7
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-02 18:29:01 -0300

    adiciona pacote exception com classe NegocioException

---
+++ src/main/java/com/msr/logfluxo/domain/exception/NegocioException.java
@@ -0,0 +1,12 @@
+package com.msr.logfluxo.domain.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@SuppressWarnings("serial")
+@AllArgsConstructor
+@Getter
+public class NegocioException extends RuntimeException{
+	
+	private String mensagem;
+}
