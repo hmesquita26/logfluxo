23a2c16e7e0d9c10d812fd0f8de52afa0d249fad
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-02 18:15:24 -0300

    adiciona classe CatalogoClienteSerive e  metodo findByEmail

---
+++ src/main/java/com/msr/logfluxo/domain/service/CatalogoClienteService.java
@@ -0,0 +1,39 @@
+package com.msr.logfluxo.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.msr.logfluxo.domain.exception.NegocioException;
+import com.msr.logfluxo.domain.model.Cliente;
+import com.msr.logfluxo.domain.repository.ClienteRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class CatalogoClienteService {
+
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Long clienteId) {
+		return clienteRepository.findById(clienteId)
+				.orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+	}
+	
+	@Transactional
+	public Cliente salvar(Cliente cliente) {
+		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail())
+				.stream()
+				.anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
+		
+		if(emailEmUso)
+			throw new NegocioException("Já existe um cliente cadastrado com este e-mail.");
+		
+		return clienteRepository.save(cliente);
+	}
+	
+	@Transactional
+	public void excluir(Long clienteId) {
+		clienteRepository.deleteById(clienteId);
+	}
+}
