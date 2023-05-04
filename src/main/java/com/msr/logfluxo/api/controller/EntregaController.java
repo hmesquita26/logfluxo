18678004a6e2ef90973a413956b508b7da3de921
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-03 22:45:15 -0300

    adiciona metodos listar e buscar a EntregaController

--- src/main/java/com/msr/logfluxo/api/controller/EntregaController.java
+++ src/main/java/com/msr/logfluxo/api/controller/EntregaController.java
@@ -1,6 +1,11 @@
 package com.msr.logfluxo.api.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +13,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msr.logfluxo.domain.model.Entrega;
+import com.msr.logfluxo.domain.repository.EntregaRepository;
 import com.msr.logfluxo.domain.service.SolicitacaoEntregaService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -17,11 +24,24 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/entregas")
 public class EntregaController {
 
+	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitacaoEntregaService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@RequestBody Entrega entrega) {
+	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
 		return solicitacaoEntregaService.solicitar(entrega);
+	}
+	
+	@GetMapping
+	public List<Entrega> listar() {
+		return entregaRepository.findAll();
+	}
+	
+	@GetMapping("/{entregaId}")
+	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
+		return entregaRepository.findById(entregaId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
