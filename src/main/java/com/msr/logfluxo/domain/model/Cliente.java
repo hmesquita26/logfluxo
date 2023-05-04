592061c8352434d5881bb0e81d2bdd30f9b7612f
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-03 22:54:41 -0300

    adiciona anotacoes para validacao

--- src/main/java/com/msr/logfluxo/domain/model/Cliente.java
+++ src/main/java/com/msr/logfluxo/domain/model/Cliente.java
@@ -6,11 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import validations.ValidationGroups;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
@@ -18,21 +21,23 @@ import lombok.Setter;
 @Entity
 public class Cliente {
 	
+	@NotNull(groups = ValidationGroups.ClienteId.class)
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotEmpty
-	@Size(min = 3, max = 50)
+	@NotBlank
+	@Size(min = 3, max = 60)
 	private String nome;
 	
-	@NotEmpty
+	@NotBlank
 	@Email
+	@Size(max = 255)
 	private String email;
 	
-	@NotEmpty
-	@Size(min = 0, max = 20)
+	@NotBlank
+	@Size(max = 20)
 	@Column(name = "fone")
 	private String telefone;
 	
