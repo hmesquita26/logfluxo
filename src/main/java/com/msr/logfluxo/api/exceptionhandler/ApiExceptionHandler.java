9111361e77e964fbf3e7ba9b59d50f7910bb470d
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-03 22:48:07 -0300

    adiciona metodo handleNegocio para tratar erro interno status 500

--- src/main/java/com/msr/logfluxo/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/msr/logfluxo/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,20 +1,24 @@
 package com.msr.logfluxo.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.msr.logfluxo.domain.exception.NegocioException;
 
 import lombok.AllArgsConstructor;
 
@@ -38,11 +42,23 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 			
 		CausaException causa = new CausaException();
 		causa.setStatus(status.value());
-		causa.setDataHora(LocalDateTime.now());
+		causa.setDataHora(OffsetDateTime.now());
 		causa.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.");
 		causa.setCampos(campos);
 		
 		return handleExceptionInternal(ex, causa, headers, status, request);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		CausaException causa = new CausaException();
+		causa.setStatus(status.value());
+		causa.setDataHora(OffsetDateTime.now());
+		causa.setTitulo(ex.getMensagem());
+		
+		return handleExceptionInternal(ex, causa, new HttpHeaders(), status, request);	
 	}
 	
 }
