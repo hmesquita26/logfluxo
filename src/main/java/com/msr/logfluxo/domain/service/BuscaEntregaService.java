4d91f0bc78c2c0898877619ae922a9d5f7d6363f
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-10 21:00:58 -0300

    adiciona buusca e registro da ocorrencia na camada de servico

---
+++ src/main/java/com/msr/logfluxo/domain/service/BuscaEntregaService.java
@@ -0,0 +1,21 @@
+package com.msr.logfluxo.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.msr.logfluxo.domain.exception.NegocioException;
+import com.msr.logfluxo.domain.model.Entrega;
+import com.msr.logfluxo.domain.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class BuscaEntregaService {
+	
+	private EntregaRepository entregaRepository;
+	
+	public Entrega buscar(Long entregaId) {
+		return entregaRepository.findById(entregaId)
+				.orElseThrow(() -> new NegocioException("Entrega não encontrada"));
+	}
+}
